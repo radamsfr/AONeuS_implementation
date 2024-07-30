5d18cc9fe27f66c91f2f223d69f8f40d12bbe19c
John Ryu Adams <radams.official1@gmail.com>
2024-07-30 11:46:36 -0700

    update AONeuS model

--- AONeuS_model.py
+++ AONeuS_model.py
@@ -1,4 +1,168 @@
-import numpy as np
 import torch
-x = torch.rand(5, 3)
-print(x)
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def render_rays(nerf_model, ray_origins, ray_directions, hn=0, hf=0.5, nb_bins=192):
+    device = ray_origins.device
+    t = torch.linspace(hn, hf, nb_bins, device=device).expand(ray_origins.shape[0], nb_bins)
+    # Perturb sampling along each ray.
+    mid = (t[:, :-1] + t[:, 1:]) / 2.
+    lower = torch.cat((t[:, :1], mid), -1)
+    upper = torch.cat((mid, t[:, -1:]), -1)
+    u = torch.rand(t.shape, device=device)
+    t = lower + (upper - lower) * u  # [batch_size, nb_bins]
+    delta = torch.cat((t[:, 1:] - t[:, :-1], torch.tensor([1e10], device=device).expand(ray_origins.shape[0], 1)), -1)
+
+    x = ray_origins.unsqueeze(1) + t.unsqueeze(2) * ray_directions.unsqueeze(1)   # [batch_size, nb_bins, 3]
+    ray_directions = ray_directions.expand(nb_bins, ray_directions.shape[0], 3).transpose(0, 1)
+
+    colors, sigma = nerf_model(x.reshape(-1, 3), ray_directions.reshape(-1, 3))
+    colors = colors.reshape(x.shape)
+    sigma = sigma.reshape(x.shape[:-1])
+
+    alpha = 1 - torch.exp(-sigma * delta)  # [batch_size, nb_bins]
+    weights = compute_accumulated_transmittance(1 - alpha).unsqueeze(2) * alpha.unsqueeze(2)
+    c = (weights * colors).sum(dim=1)  # Pixel values
+    weight_sum = weights.sum(-1).sum(-1)  # Regularization for white background
+    return c + 1 - weight_sum.unsqueeze(-1)
+
+
+
+
+def compute_accumulated_transmittance(alphas):
+    accumulated_transmittance = torch.cumprod(alphas, 1)
+    return torch.cat((torch.ones((accumulated_transmittance.shape[0], 1), device=alphas.device),
+                      accumulated_transmittance[:, :-1]), dim=-1)
+    
+    
+    
+class NerfModel(nn.Module):
+    def __init__(self, embedding_dim_pos=10, embedding_dim_direction=4, hidden_dim=64):
+        super(NerfModel, self).__init__()
+
+        self.blockN = nn.Sequential(nn.Linear(embedding_dim_pos * 3, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), )
+
+        self.Fson = 64
+        self.Fcam = 64
+        self.N = 1
+        self.GradN = 3
+        self.blockNOut = nn.Sequential(nn.Linear(hidden_dim, self.Fson + self.N + self.GradN + self.Fcam), nn.ReLU(), )
+        
+        self.Dcam = 2
+        self.Dson = 2
+        self.blockSonar = nn.Sequential(nn.Linear(self.Fson + self.N + self.GradN + (self.Dson * embedding_dim_direction), hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),)
+        
+        self.blockSonarOut = nn.Sequential(nn.Linear(hidden_dim, 1), nn.Sigmoid(), )
+        
+        self.blockCamera = nn.Sequential(nn.Linear(self.Fcam + self.N + self.GradN + (self.Dcam * embedding_dim_direction), hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),)
+        
+        self.blockCameraOut = nn.Sequential(nn.Linear(hidden_dim, 3), nn.Sigmoid(), )
+
+
+        self.embedding_dim_pos = embedding_dim_pos
+        self.embedding_dim_direction = embedding_dim_direction
+        self.relu = nn.ReLU()
+
+    @staticmethod
+    def positional_encoding(x, L):
+        out = [x]
+        for j in range(L):
+            out.append(torch.sin(2 ** j * x))
+            out.append(torch.cos(2 ** j * x))
+        return torch.cat(out, dim=1)
+
+    def forward(self, o, d):
+        emb_x = self.positional_encoding(o, self.embedding_dim_pos)
+        emb_d = self.positional_encoding(d, self.embedding_dim_direction)
+        
+        h = self.blockN(emb_x)
+        h = self.blockNOut(h)
+        
+        Sonar = self.blockSonar(torch.cat(h[:(self.Fson + self.N + self.GradN)], emb_d), dim=1)
+        Sonar = self.blockSonarOut(Sonar)
+        
+        Camera = self.blockCamera(torch.cat(h[-(self.Fcam + self.N + self.GradN):], emb_d), dim=1)
+        Camera = self.blockCameraOut(Camera)
+        
+        return Sonar, Camera
+        
+        # tmp = self.block2(torch.cat((h, emb_x), dim=1))
+        # h, sigma = tmp[:, :-1], self.relu(tmp[:, -1])
+        # h = self.block3(torch.cat((h, emb_d), dim=1))
+        # c = self.block4(h)
+        # return c, sigma
+    
+    
+    
+def train(nerf_model, optimizer, scheduler, data_loader, device='cpu', hn=0, hf=1, 
+          nb_epochs=int(1e5), nb_bins=192, H=400, W=400):
+    training_loss = []
+    for _ in tqdm(range(nb_epochs)):
+        for batch in data_loader:
+            ray_origins = batch[:, :3].to(device)
+            ray_directions = batch[:, 3:6].to(device)
+            ground_truth_px_values = batch[:, 6:].to(device)
+
+            regenerated_px_values = render_rays(nerf_model, ray_origins, ray_directions, hn=hn, hf=hf, nb_bins=nb_bins)
+            loss = ((ground_truth_px_values - regenerated_px_values) ** 2).sum()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            training_loss.append(loss.item())
+        scheduler.step()
+
+        for img_index in range(200):
+            test(hn, hf, testing_dataset, img_index=img_index, nb_bins=nb_bins, H=H, W=W)
+
+    return training_loss
+
+
+
+@torch.no_grad()
+def test(hn, hf, dataset, chunk_size=10, img_index=0, nb_bins=192, H=400, W=400):
+    ray_origins = dataset[img_index * H * W: (img_index + 1) * H * W, :3]
+    ray_directions = dataset[img_index * H * W: (img_index + 1) * H * W, 3:6]
+
+    data = []
+    for i in range(int(np.ceil(H / chunk_size))):
+        ray_origins_ = ray_origins[i * W * chunk_size: (i + 1) * W * chunk_size].to(device)
+        ray_directions_ = ray_directions[i * W * chunk_size: (i + 1) * W * chunk_size].to(device)
+
+        regenerated_px_values = render_rays(model, ray_origins_, ray_directions_, hn=hn, hf=hf, nb_bins=nb_bins)
+        data.append(regenerated_px_values)
+    img = torch.cat(data).data.cpu().numpy().reshape(H, W, 3)
+
+    plt.figure()
+    plt.imshow(img)
+    plt.savefig(f'novel_views/img_{img_index}.png', bbox_inches='tight')
+    plt.close()
+    
+    
+    
+if __name__ == 'main':
+    device = 'cuda'
+    training_dataset = torch.from_numpy(np.load('training_data.pkl', allow_pickle=True))
+    testing_dataset = torch.from_numpy(np.load('testing_data.pkl', allow_pickle=True))
+    model = NerfModel(hidden_dim=256).to(device)
+    model_optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(model_optimizer, milestones=[2, 4, 8], gamma=0.5)
+
+    data_loader = DataLoader(training_dataset, batch_size=1024, shuffle=True)
+    train(model, model_optimizer, scheduler, data_loader, nb_epochs=16, device=device, hn=2, hf=6, nb_bins=192, H=400,
+          W=400)
